4c3ce9cd7960d1cc297416ca5af49215d652e0e2
lizhuan1 <lizhuan1@GB144WS02.WIN.ecf.utoronto.ca>
2016-01-20 14:49:20 -0500

    commit on netbeans

--- src/Network361/mainclass.java
+++ src/Network361/mainclass.java
@@ -1,6 +1,7 @@
 package Network361;
 import java.io.*;
 public class mainclass {
+        //sadasdas
 	public static void main(String[] args) throws IOException{
 		FTPServer newFTPServer = new FTPServer("JianJian");
 		newFTPServer.ServerRun();
